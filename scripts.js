c9d93c0178e15e937e52a56fb43e2739e6cf3438
Vitorblack82 <apvitor@gmail.com>
2025-05-03 23:03:53 -0300

    alteração min, max

--- scripts.js
+++ scripts.js
@@ -5,13 +5,21 @@
 
 const chave = "f7827a1c83aabfe7899c0ac2e5d1020b"
 
+
+
+
+
+
 function colocarNaTela(dados) {
 
     document.querySelector(".text-principal").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C "
     document.querySelector(".tempo").innerHTML = dados.weather[0].description
     document.querySelector(".nuvem").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-    document.querySelector(".tipoDeAr").innerHTML = "Umidade:  "  + dados.main.humidity + " %"
+    document.querySelector(".tipoDeAr").innerHTML = "Umidade:  " + dados.main.humidity + " %"
+    document.querySelector(".minima").innerHTML = "Min." + Math.floor(dados.main.temp_min) + "°"
+    document.querySelector(".maxima").innerHTML = "Máx." + Math.floor(dados.main.temp_max) + "°"
+console.log(dados)
 }
 
 async function BuscarCidade(cidade) {
@@ -22,6 +30,7 @@ async function BuscarCidade(cidade) {
         chave +
         "&lang=pt_br" +
         "&units=metric")
+
         .then(resposta => resposta.json())
 
 
@@ -38,3 +47,5 @@ function cliqueiNoBotao() {
 
     BuscarCidade(cidade)
 }
+
+
